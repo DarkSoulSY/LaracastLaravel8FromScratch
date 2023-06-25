a99202196fc8b02cdecbab911b33697d9227eb15
Nasser Al-Abdullah <nasser-alabdullah@outlook.com>
2023-06-25 08:54:04 +0300

    Before transitioning to a database

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    // $posts = array_map(function ($file) {
+    //     $document = YamlFrontMatter::parseFile($file);
+
+    //     return new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->slug,
+    //         $document->body()
+
+
+    //     );
+    // }, $files);
+
+
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::findOrFail($slug)]
+    );
+});
